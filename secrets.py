a66c5a6e4ae64a5552b7f64b4433df68c474dd71
Toxillo <33227102+Toxillo@users.noreply.github.com>
2022-09-14 16:04:45 +0200

    Removed secrets

--- secrets.py
+++ secrets.py
@@ -1,3 +1,3 @@
-client_id = '59051dc1dadc4d349d40a6595e53a552'
-client_secret = '0e852f30aa894fb38ae2cd976e8753b0'
-spotify_user_id = 'toxiiio'
+client_id = ''
+client_secret = ''
+spotify_user_id = ''
